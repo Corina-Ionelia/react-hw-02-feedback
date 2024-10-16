cab1bcad04343711bdb34bca31d4812973cf4ca7
Corina Ionelia <corynaionelia@gmail.com>
2024-10-16 16:32:10 +0300

    a

--- src/components/Notification/Notification.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './notification.module.css';
-
-function Notification({ message }) {
-  return <p className={styles.message}>{message}</p>;
-}
-
-export default Notification;
